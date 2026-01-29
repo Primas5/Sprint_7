7f3b1271bed30f278576b8c38035727c17860ee9
Ваше Имя Фамилия <your_email@example.com>
2026-01-29 16:27:17 +0300

    исправлена работа фикстуры

--- conftest.py
+++ conftest.py
@@ -4,13 +4,9 @@ from methods.courier_methods import CourierMeth
 
 @pytest.fixture
 def create_courier_and_delete():
-    """Фикстура для создания и удаления курьера"""
+    """Фикстура для удаления курьера"""
     data = generate_data_for_courier()
-
-    st_code,create_result = CourierMeth().create_courier(data[0],data[1],data[2])
-    if st_code != 201:
-        pytest.skip(f"Не удалось создать курьера: {st_code}")
-    yield st_code, create_result, data
+    yield data
     currentcode, currentdata = CourierMeth().login_courier(data[0], data[1])
     currentid = currentdata["id"]
     CourierMeth().delete_courier(currentid)
--- tests/couriers/test_create_courier.py
+++ tests/couriers/test_create_courier.py
@@ -7,17 +7,27 @@ class TestCourierRegister:
 
     @allure.title("Создание курьера")
     def test_create_courier_is_created(self, create_courier_and_delete):
-        st_code, create_result, courier_data = create_courier_and_delete
+        courier_data = create_courier_and_delete
+        st_code, create_result = CourierMeth().create_courier(
+            courier_data[0],
+            courier_data[1],
+            courier_data[2]
+        )
         assert (st_code == 201)
         assert (create_result == Data.COURIER_CREATION_ANSWER)
 
     @allure.title("Создание курьера с тем же логином")
     def test_creating_courier_with_the_same_login_is_denied(self, create_courier_and_delete):
-        st_code, create_result, courier_data = create_courier_and_delete
+        courier_data = create_courier_and_delete
         created_courier = CourierMeth().create_courier(
             courier_data[0],
             courier_data[1],
-            courier_data[2],
+            courier_data[2]
+        )
+        created_courier = CourierMeth().create_courier(
+            courier_data[0],
+            courier_data[1],
+            courier_data[2]
         )
         assert (created_courier[0] == 409)
         assert (created_courier[1] == Data.COURIER_ALREADY_EXIST_ANSWER)
@@ -47,16 +57,12 @@ class TestCourierRegister:
         assert (created_courier[1] == Data.COURIER_CREATION_MISSED_DATA_ANSWER)
 
     @allure.title("Создание курьера без имени")
-    def test_creating_courier_without_first_name_is_denied(self):
-        data_courier = generate_data_for_courier()
+    def test_creating_courier_without_first_name_is_denied(self,create_courier_and_delete):
+        data_courier = create_courier_and_delete
         created_courier = CourierMeth().create_courier(
             data_courier[0],
             data_courier[1],
-            "",
+            ""
         )
         assert (created_courier[0] == 201)
         assert (created_courier[1] == Data.COURIER_CREATION_ANSWER)
-        # Удалить созданного курьера
-        currentcode, currentdata = CourierMeth().login_courier(data_courier[0], data_courier[1])
-        currentid = currentdata["id"]
-        CourierMeth().delete_courier(currentid)
